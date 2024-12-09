c79401907944abd3ee1097513f27f135cc0e371f
Tomer <tomere1111@hotmail.com>
2024-12-09 18:58:22 +0200

    Inital commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  
+  return (
+    <div>
+      <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-red">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+    </div>
+  )
+}
+
+export default App
